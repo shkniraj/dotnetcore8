0ef6a010afc044f332d918ab901f5c30d4534a2a
NIRAJ KUMAR <sk.niraj3128@gmail.com>
2025-08-06 17:11:43 +0530

    Create Customer.cs

---
+++ PrimaryConstructorDemo/Customer.cs
@@ -0,0 +1,10 @@
+namespace PrimaryConstructorsDemo.Models;
+
+// Primary constructor without property exposure
+public class Customer(string name, int age)
+{
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Customer: {name}, Age: {age}");
+    }
+}
