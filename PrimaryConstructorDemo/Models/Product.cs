12097a3ceca6951d7d4a4293909c51be569f5339
NIRAJ KUMAR <sk.niraj3128@gmail.com>
2025-08-06 17:13:27 +0530

    Create Product.cs

---
+++ PrimaryConstructorDemo/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace PrimaryConstructorsDemo.Models;
+
+// Primary constructor with public property exposure
+public class Product(string name, decimal price)
+{
+    public string Name => name;
+    public decimal Price => price;
+
+    public string GetDetails() => $"{Name} costs ₹{Price}";
+}
